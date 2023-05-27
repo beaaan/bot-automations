d1f3d9193ad28e244a899379f9996ca69e4aa9bb
beaaan <vietkhanh2007@gmail.com>
2023-05-27 11:34:19 +0700

    Fixed

--- run.bat
+++ run.bat
@@ -1,2 +1,21 @@
 @echo off
-node index
+if _%1_==_payload_  goto :payload
+
+:getadmin
+    echo %~nx0: elevating self
+    set vbs=%temp%\getadmin.vbs
+    echo Set UAC = CreateObject^("Shell.Application"^)                >> "%vbs%"
+    echo UAC.ShellExecute "%~s0", "payload %~sdp0 %*", "", "runas", 1 >> "%vbs%"
+    "%temp%\getadmin.vbs"
+    del "%temp%\getadmin.vbs"
+goto :eof
+
+:payload
+
+::ENTER YOUR CODE BELOW::
+%~d0
+cd %~dp0
+node index.js
+::END OF YOUR CODE::
+
+pause
